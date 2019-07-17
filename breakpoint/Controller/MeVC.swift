0cebde0592083dd9203cfb435eb9ad8a003ee833
Anthony <cozziad@gmail.com>
2019-07-17 19:52:23 -0400

    Lecture 145 Done

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,28 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Anthony Cozzi on 7/17/19.
+//  Copyright © 2019 Anthony Cozzi. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var emailLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func logoutBtnPressed(_ sender: Any) {
+    }
+    
+
+}
